3448486973cffb1adc304e337c5432911ad685b6
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-08-03 23:59:34 +0700

    update post crud operation

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 
 @Resolver()
@@ -6,5 +6,41 @@ export class PostResolver {
 	@Query(() => [Post])
 	async posts(): Promise<Post[]> {
 		return Post.find();
+	}
+
+	@Query(() => Post, { nullable: true })
+	async post(@Arg("id") id: number): Promise<Post | undefined> {
+		return Post.findOne(id);
+	}
+
+	@Mutation(() => Post)
+	async createPost(@Arg("title") title: string): Promise<Post> {
+		return Post.create({
+			title,
+		}).save();
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg("id") id: number,
+		@Arg("title") title: string
+	): Promise<Post | null> {
+		const post = await Post.findOne(id);
+		if (!post) {
+			return null;
+		}
+
+		if (typeof title !== undefined) {
+			post.title = title;
+			Post.update(id, post);
+		}
+
+		return post;
+	}
+
+	@Mutation(() => Boolean)
+	async deletePost(@Arg("id", () => Int) id: number): Promise<boolean> {
+		await Post.delete({ id });
+		return true;
 	}
 }
