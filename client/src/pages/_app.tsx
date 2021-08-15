9f3530ec3fef6246be3a7dad5ac5c5f5a24dc2b8
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-08-15 13:08:03 +0700

    update cache for logout mutation

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
 import { AppProps } from "next/app";
 import React from "react";
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import {
+	CurrentUserDocument,
+	CurrentUserQuery,
+	LogoutMutation,
+} from "../generated/graphql";
 import theme from "../theme";
+
+function betterUpdateQuery<Result, Query>(
+	cache: Cache,
+	qi: QueryInput,
+	result: any,
+	fn: (r: Result, q: Query) => Query
+) {
+	return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
 	const client = createClient({
@@ -10,6 +25,24 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
 		fetchOptions: {
 			credentials: "include",
 		},
+		exchanges: [
+			dedupExchange,
+			cacheExchange({
+				updates: {
+					Mutation: {
+						logout: (result, args, cache, info) => {
+							betterUpdateQuery<LogoutMutation, CurrentUserQuery>(
+								cache,
+								{ query: CurrentUserDocument },
+								result,
+								() => ({ currentUser: null })
+							);
+						},
+					},
+				},
+			}),
+			fetchExchange,
+		],
 	});
 
 	return (
